817c672835c3cd87c73fc518cbac39c5ad4e1551
caowenbin <caowenbin@360.cn>
2020-06-09 15:33:57 +0800

    log

--- src/Alog.js
+++ src/Alog.js
@@ -2,14 +2,33 @@
  * @Author: caowenbin 
  * @Date: 2020-06-09 10:28:38 
  * @Last Modified by: caowenbin
- * @Last Modified time: 2020-06-09 15:07:13
+ * @Last Modified time: 2020-06-09 15:33:54
  */
 const height_item = 42;     // 查询条件，一行
 const height_input = 32;    // 输入框
 const width_label = 80;    // label 宽度
 const width_page = 414;     // 页面宽度
-
 const width_sep = 12;
+
+const Helper = {
+  /**
+	 * 参数序列化
+	 * @param {Object} data 
+	 */
+  param(data) {
+    if (!data) {
+      return "";
+    }
+
+    let keys = Object.keys(data);
+    let params = [];
+    keys.map((key) => {
+      params.push(key + "=" + encodeURIComponent(data[key]));
+    });
+    return params.join("&");
+  },
+}
+
 
 // 查询条件
 let search = {
@@ -40,122 +59,94 @@ let renderData = {
 		title: 'ALog',
   },
   views : [
-    {
-      type: "view",
-      info: "查询条件列表",
-      props: {
-        id : "search_box",
-      },
-      layout: function(make, view) {
-
-      },
-      views : [],
-    },
-    {
-      type: "button",
-      info: "查询条件按钮",
-      props: {
-        id : "search_btn",
-        title : "查询",
-      },
-      layout: function(make, view) {
-        make.left.right.inset(width_sep);
-        make.top.equalTo(Object.keys(search).length * height_item + width_sep);
-      },
-      events: {
-        tapped: function(sender) {
-          $http.get({
-            url: "https://www.fastmock.site/mock/b4a46c87101afae11fcf7ef792ddfe90/alog/getData",
-            handler: function(resp) {
-              // test
-              // console.log("get", resp);
-
-              let res = resp.data;
-
-              // test
-              // console.log(typeof res, res);
-
-              if(!res || res.errno != 0){
-                $ui.toast(res && res.errmsg || '请求报错');
-                return;
-              }
-
-              let _data = res.data;
-              let list = [];
-              _data.forEach(item => {
-                item = {...item};
-                // 排序
-                ["time", "du", "c"].forEach(key => {
-                  list.push(key + ": " + item[key]);
-                  delete(item[key]);
-                });
-                // 其它
-                Object.keys(item).forEach(key => {
-                  list.push(key + ": " + item[key]);
-                });
-                list.push("");
-              });
-
-              $ui.toast('已复制' + _data.length + "条日志");
-              $clipboard.text = list.join("\n");
-            }
-          })
-        },
-      },
-    },
+    
   ],
 };
 
 // 查询条件
 Object.keys(search).forEach((k, index) => {
-  let item = {
-    type : "view",
-    info : "查询条件，行",
-    props : {
-      id : "query_" + k,
-      class : "query_line",
+  let _height = height_item * index;
+
+  // 自测 views 嵌套时，input 呼不起键盘
+  renderData.views.push({
+    type: "label",
+    props: {
+      text: k,
+      align: $align.right,
     },
     layout: function(make, view) {
+      make.top.equalTo(_height);
+      make.width.equalTo(width_label);
       make.height.equalTo(height_item);
-      make.top.equalTo(height_item * index);
+    }
+  });
+  renderData.views.push({
+    type: "input",
+    props: {
+      text: search[k],
+      placeholder: k,
     },
-    views: [
-      {
-        type: "label",
-        props: {
-          text: k,
-          align: $align.right,
-        },
-        layout: function(make, view) {
-          make.width.equalTo(width_label);
-          make.height.equalTo(height_item);
+    layout: function(make, view) {
+      make.width.equalTo(width_page - width_label - width_sep * 3);
+      make.height.equalTo(height_input);
+      make.top.equalTo(_height + (height_item - height_input) / 2);
+      make.left.equalTo(width_label + width_sep * 2);
+    },
+    events : {
+      changed: function(sender) {
+        search[k] = sender.text;
+      },
+    },
+  });
+});
+// 查询按钮
+renderData.views.push({
+  type: "button",
+  info: "查询条件按钮",
+  props: {
+    id : "search_btn",
+    title : "查询",
+  },
+  layout: function(make, view) {
+    make.left.right.inset(width_sep);
+    make.top.equalTo(Object.keys(search).length * height_item + width_sep);
+  },
+  events: {
+    tapped: function(sender) {
+      $http.get({
+        // mock
+        // url: "https://www.fastmock.site/mock/b4a46c87101afae11fcf7ef792ddfe90/alog/getData?" + Helper.param(search),
+        url: "http://alog.frostbelt.cn/getData?" + Helper.param(search),
+        handler: function(resp) {
+          let res = resp.data;
+
+          if(!res || res.errno != 0){
+            $ui.toast(res && res.errmsg || '请求报错');
+            return;
+          }
+
+          let _data = res.data;
+          let list = [];
+          _data.forEach(item => {
+            item = {...item};
+            // 排序
+            ["time", "du", "c"].forEach(key => {
+              list.push(key + ": " + item[key]);
+              delete(item[key]);
+            });
+            // 其它
+            Object.keys(item).forEach(key => {
+              list.push(key + ": " + item[key]);
+            });
+            list.push("");
+          });
+
+          $clipboard.text = list.join("\n");
+          $ui.toast('已复制' + _data.length + "条日志");
         }
-      },
-      {
-        type: "input",
-        props: {
-          text: search[k],
-          placeholder: k,
-        },
-        layout: function(make, view) {
-          make.width.equalTo(width_page - width_label - width_sep * 3);
-          make.height.equalTo(height_input);
-          make.top.equalTo((height_item - height_input) / 2);
-          make.left.equalTo(width_label + width_sep * 2);
-        },
-        events : {
-          changed: function(sender) {
-            // test
-            console.log(sender);
-
-            search[k] = sender;
-          },
-        },
-      },
-    ],
-  };
-
-  renderData.views.filter(item => item.props.id == "search_box")[0].views.push(item);
+      })
+    },
+  },
 });
 
 $ui.render(renderData);
